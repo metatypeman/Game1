12238923aba0171b1dab7a2c2fb3661f92937b90
Sergiy Tolkachov <metatypeman@gmail.com>
2018-01-07 20:52:13 +0200

    snapshot

--- Assets/MyThirdPersonUserControl.cs
+++ Assets/MyThirdPersonUserControl.cs
@@ -14,58 +14,58 @@ public class MyThirdPersonUserControl : MonoBehaviour {
 
                                               // Use this for initialization
     void Start () {
-        // get the transform of the main camera
-        if (Camera.main != null)
-        {
-            m_Cam = Camera.main.transform;
-        }
-        else
-        {
-            Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-            // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-        }
+        //// get the transform of the main camera
+        //if (Camera.main != null)
+        //{
+        //    m_Cam = Camera.main.transform;
+        //}
+        //else
+        //{
+        //    Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+        //    // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+        //}
 
-        // get the third person character ( this should never be null due to require component )
-        m_Character = GetComponent<MyThirdPersonCharacter>();
+        //// get the third person character ( this should never be null due to require component )
+        //m_Character = GetComponent<MyThirdPersonCharacter>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!m_Jump)
-        {
-            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-        }
+        //if (!m_Jump)
+        //{
+        //    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        //}
     }
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        Debug.LogWarning("FixedUpdate");
+//        Debug.LogWarning("FixedUpdate");
 
-        // read inputs
-        var h = CrossPlatformInputManager.GetAxis("Horizontal");
-        var v = CrossPlatformInputManager.GetAxis("Vertical");
-        var crouch = Input.GetKey(KeyCode.C);
+//        // read inputs
+//        var h = CrossPlatformInputManager.GetAxis("Horizontal");
+//        var v = CrossPlatformInputManager.GetAxis("Vertical");
+//        var crouch = Input.GetKey(KeyCode.C);
 
-        // calculate move direction to pass to character
-        if (m_Cam != null)
-        {
-            // calculate camera relative direction to move:
-            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-            m_Move = v * m_CamForward + h * m_Cam.right;
-        }
-        else
-        {
-            // we use world-relative directions in the case of no main camera
-            m_Move = v * Vector3.forward + h * Vector3.right;
-        }
-#if !MOBILE_INPUT
-        // walk speed multiplier
-        if (Input.GetKey(KeyCode.LeftShift)) { m_Move *= 0.5f; }
-#endif
+//        // calculate move direction to pass to character
+//        if (m_Cam != null)
+//        {
+//            // calculate camera relative direction to move:
+//            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+//            m_Move = v * m_CamForward + h * m_Cam.right;
+//        }
+//        else
+//        {
+//            // we use world-relative directions in the case of no main camera
+//            m_Move = v * Vector3.forward + h * Vector3.right;
+//        }
+//#if !MOBILE_INPUT
+//        // walk speed multiplier
+//        if (Input.GetKey(KeyCode.LeftShift)) { m_Move *= 0.5f; }
+//#endif
 
-        // pass all parameters to the character control script
-        m_Character.Move(m_Move, crouch, m_Jump);
-        m_Jump = false;
+//        // pass all parameters to the character control script
+//        m_Character.Move(m_Move, crouch, m_Jump);
+//        m_Jump = false;
     }
 }
