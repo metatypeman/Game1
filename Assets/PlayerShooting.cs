b2ea3cf34341fe183c4c8594f34966b7fda11b40
Sergiy Tolkachov <metatypeman@gmail.com>
2018-02-03 16:29:46 +0200

    snapshot

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    public GameObject Body;
+
+    ParticleSystem gunParticles;                    // Эффект выстрела
+    LineRenderer gunLine;                           // Линия выстрела
+    AudioSource gunAudio;                           // Звук выстрела
+    Light gunLight;
+    public Light faceLight;
+
+    Ray shootRay;
+
+    public float range = 100f;
+    RaycastHit shootHit;
+
+    // Use this for initialization
+    void Start () {
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+
+        Shoot();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gunLine.SetPosition(0, transform.position);
+        // Настройка луча
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range))
+        {
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+
+    public void Shoot()
+    {
+        gunAudio.Play();
+
+        // Включаем всвет
+        gunLight.enabled = true;
+        faceLight.enabled = true;
+
+        gunParticles.Stop();
+        gunParticles.Play();
+
+        // Линия выстрела
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+        // Настройка луча
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+    }
+}
