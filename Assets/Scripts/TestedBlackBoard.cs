5e8cbfbd7a73e6cc6bbadf4d7318757f9fde0275
Sergiy Tolkachov <metatypeman@gmail.com>
2018-04-18 09:18:38 +0300

    snapshot

--- Assets/Scripts/TestedBlackBoard.cs
+++ Assets/Scripts/TestedBlackBoard.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Assets.Scripts
 {
-    public class TestedBlackBoard
+    public class TestedBlackBoard : IObjectToString
     {
+        public int InstanceIdOfRifle { get; set; }
+        
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+        
+        public string PropertiesToSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
+            var nextSpaces = StringHelper.Spaces(nextN);
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(InstanceIdOfRifle)} = {InstanceIdOfRifle}");
+            return sb.ToString();
+        }
     }
 }
