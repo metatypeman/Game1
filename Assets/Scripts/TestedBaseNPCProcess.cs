2e0ed1de49e0225d70b6587f76081b525e5ab040
Sergiy Tolkachov <metatypeman@gmail.com>
2018-04-20 10:32:35 +0300

    snapshot

--- Assets/Scripts/TestedBaseNPCProcess.cs
+++ Assets/Scripts/TestedBaseNPCProcess.cs
@@ -9,5 +9,38 @@ namespace Assets.Scripts
 {
     public class TestedBaseNPCProcess: BaseNPCProcessWithBlackBoard<TestedBlackBoard>
     {
+        protected void GoToTargetWayPoint(string nameOfThisWaypoint, bool withWaiting = true)
+        {
+#if UNITY_EDITOR
+            //Debug.Log($"TestedRunAwayNPCProcess Begin GoToTargetWayPoint nameOfThisWaypoint = {nameOfThisWaypoint} withWaiting = {withWaiting}");
+#endif
+
+            var targetWayPoint = WaypointsBus.GetByName(nameOfThisWaypoint);
+
+            if (targetWayPoint != null)
+            {
+                var moveCommand = new HumanoidHStateCommand();
+                moveCommand.State = HumanoidHState.Walk;
+                moveCommand.TargetPosition = targetWayPoint.Position;
+
+#if UNITY_EDITOR
+                //Debug.Log($"TestedRunAwayNPCProcess GoToTargetWayPoint moveCommand = {moveCommand}");
+#endif
+                var tmpTask = ExecuteBody(moveCommand);
+                //mTmpTask = tmpTask;
+#if UNITY_EDITOR
+                //Debug.Log($"TestedRunAwayNPCProcess GoToTargetWayPoint tmpTask = {tmpTask}");
+#endif
+
+                if (withWaiting)
+                {
+                    Wait(tmpTask);
+                }
+            }
+
+#if UNITY_EDITOR
+            //Debug.Log("End TestedRunAwayNPCProcess GoToTargetWayPoint");
+#endif
+        }
     }
 }
