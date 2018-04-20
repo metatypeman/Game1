f2e39edfc90f73f7d5c4d713fe714d8c1f706e87
Sergiy Tolkachov <metatypeman@gmail.com>
2018-04-20 13:54:27 +0300

    snapshot

--- Assets/Scripts/TestedNPCProcesses.cs
+++ Assets/Scripts/TestedNPCProcesses.cs
@@ -19,6 +19,7 @@ namespace Assets.Scripts
 #endif
 
             var command = TestedInspectingNPCProcess.CreateCommand();
+            command.Priority = NPCProcessPriorities.Highest;
             Execute(command);
         }
 
