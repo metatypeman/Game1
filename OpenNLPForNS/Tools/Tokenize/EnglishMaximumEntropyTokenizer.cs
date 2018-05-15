aac328cc3e60e0d873cd6d373b1dd9709cf3ecbc
Sergiy Tolkachov <metatypeman@gmail.com>
2018-05-15 16:18:05 +0300

    snapshot

---
+++ OpenNLPForNS/Tools/Tokenize/EnglishMaximumEntropyTokenizer.cs
@@ -0,0 +1,20 @@
+﻿//OpenNLPForNS is based on https://github.com/AlexPoint/OpenNlp
+//I just need OpenNLP what is based on Net. Standard 1.6. 
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenNLP.Tools.Tokenize
+{
+    /// <summary> 
+    /// A tokenizer which uses default English data for the maximum entropy model.
+    /// </summary>
+    public class EnglishMaximumEntropyTokenizer : MaximumEntropyTokenizer
+    {
+        public EnglishMaximumEntropyTokenizer(string name) : base(new SharpEntropy.GisModel(new SharpEntropy.IO.BinaryGisModelReader(name)))
+        {
+            AlphaNumericOptimization = true;
+        }
+    }
+}
---
+++ SharpEntropyForNS/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SharpEntropyForNS
+{
+    public class Class1
+    {
+    }
+}
