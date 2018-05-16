d9b3dfe70018e39e489008319abd02a77debbd2c
Sergiy Tolkachov <metatypeman@gmail.com>
2018-05-16 10:32:26 +0300

    snapshot

---
+++ OpenNLPForNS/Tools/Ling/IHasIndex.cs
@@ -0,0 +1,29 @@
+﻿//OpenNLPForNS is based on AlexPoint/OpenNlp
+//I just need OpenNLP for Net. Standard 1.6.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenNLP.Tools.Ling
+{
+    /// <summary>
+    /// @author grenager
+    /// 
+    /// Code retrieved on the Stanford parser and ported to C# (see http://nlp.stanford.edu/software/lex-parser.shtml)
+    /// </summary>
+    public interface IHasIndex
+    {
+        string DocId();
+
+        void SetDocId(string docId);
+
+        int SentIndex();
+
+        void SetSentIndex(int sentIndex);
+
+        int Index();
+
+        void SetIndex(int index);
+    }
+}
