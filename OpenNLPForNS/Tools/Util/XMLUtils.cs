32a10a7ee0e45c7e322632efcd9661cc575eb187
Sergiy Tolkachov <metatypeman@gmail.com>
2018-05-17 15:02:37 +0300

    snapshot

---
+++ OpenNLPForNS/Tools/Util/XMLUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+
+namespace OpenNLP.Tools.Util
+{
+    /// <summary>
+    /// Provides some utilities for dealing with XML files, both by properly 
+    /// parsing them and by using the methods of a desperate Perl hacker.
+    /// </summary>
+    public class XmlUtils
+    {
+
+        /// <summary>
+        /// Escape an unescaped xml string
+        /// </summary>
+        public static string XmlEscape(string unescaped)
+        {
+            var doc = new XmlDocument();
+            XmlNode node = doc.CreateElement("root");
+            node.InnerText = unescaped;
+            return node.InnerXml;
+        }
+    }
+}
