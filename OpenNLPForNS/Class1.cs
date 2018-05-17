7fb78894815afafe5a05dba99cca3652a13447cd
Sergiy Tolkachov <metatypeman@gmail.com>
2018-05-17 13:28:30 +0300

    snapshot

--- OpenNLPForNS/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OpenNLPForNS
-{
-    public class Class1
-    {
-    }
-}
--- SharpEntropyForNS/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SharpEntropyForNS
-{
-    public class Class1
-    {
-    }
-}
