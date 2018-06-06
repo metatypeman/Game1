28ebd085e43009573acfdf307d6e098304f55f58
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-06 09:09:42 +0300

    snapshot

---
+++ MyNPCLib/PersistLogicalData/RuleInstance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNPCLib.PersistLogicalData
+{
+    /// <summary>
+    /// Represents instance of rule (or fact) in the storage.
+    /// </summary>
+    [Serializable]
+    public class RuleInstance: IObjectToString, IShortObjectToString
+    {
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        public string PropertiesToSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+
+        public string ToShortString()
+        {
+            return ToShortString(0u);
+        }
+
+        public string ToShortString(uint n)
+        {
+            return this.GetDefaultToShortStringInformation(n);
+        }
+
+        public string PropertiesToShortSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+    }
+}
