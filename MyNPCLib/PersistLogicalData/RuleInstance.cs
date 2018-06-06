03beebc1d59fb4f646760d968ba00777cffd7429
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-06 15:58:07 +0300

    snapshot

--- MyNPCLib/PersistLogicalData/RuleInstance.cs
+++ MyNPCLib/PersistLogicalData/RuleInstance.cs
@@ -10,6 +10,13 @@ namespace MyNPCLib.PersistLogicalData
     [Serializable]
     public class RuleInstance: IObjectToString, IShortObjectToString
     {
+        public string Name { get; set; }
+        public ulong Key { get; set; }
+        public bool IsPart_1_Active { get; set; }
+        public bool IsPart_2_Active { get; set; }
+        public RulePart Part_1 { get; set; }
+        public RulePart Part_2 { get; set; }
+
         public override string ToString()
         {
             return ToString(0u);
@@ -25,7 +32,31 @@ namespace MyNPCLib.PersistLogicalData
             var spaces = StringHelper.Spaces(n);
             var nextN = n + 4;
             var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Name)} = {Name}");
+            sb.AppendLine($"{spaces}{nameof(Key)} = {Key}");
+            sb.AppendLine($"{spaces}{nameof(IsPart_1_Active)} = {IsPart_1_Active}");
+            sb.AppendLine($"{spaces}{nameof(IsPart_2_Active)} = {IsPart_2_Active}");
+            if(Part_1 == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Part_1)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Part_1)}");
+                sb.Append(Part_1.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Part_1)}");
+            }
 
+            if(Part_2 == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Part_2)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Part_2)}");
+                sb.Append(Part_2.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Part_2)}");
+            }
             return sb.ToString();
         }
 
@@ -42,8 +73,33 @@ namespace MyNPCLib.PersistLogicalData
         public string PropertiesToShortSting(uint n)
         {
             var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
             var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Name)} = {Name}");
+            sb.AppendLine($"{spaces}{nameof(Key)} = {Key}");
+            sb.AppendLine($"{spaces}{nameof(IsPart_1_Active)} = {IsPart_1_Active}");
+            sb.AppendLine($"{spaces}{nameof(IsPart_2_Active)} = {IsPart_2_Active}");
+            if (Part_1 == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Part_1)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Part_1)}");
+                sb.Append(Part_1.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Part_1)}");
+            }
 
+            if (Part_2 == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Part_2)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Part_2)}");
+                sb.Append(Part_2.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Part_2)}");
+            }
             return sb.ToString();
         }
     }
--- MyNPCLib/PersistLogicalData/RulePart.cs
+++ MyNPCLib/PersistLogicalData/RulePart.cs
@@ -7,6 +7,10 @@ namespace MyNPCLib.PersistLogicalData
     [Serializable]
     public class RulePart : IObjectToString, IShortObjectToString
     {
+        public bool IsActive { get; set; }
+        public RuleInstance Parent { get; set; }
+        public RulePart NextPart { get; set; }
+
         public override string ToString()
         {
             return ToString(0u);
@@ -22,6 +26,29 @@ namespace MyNPCLib.PersistLogicalData
             var spaces = StringHelper.Spaces(n);
             var nextN = n + 4;
             var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(IsActive)} = {IsActive}");
+
+            if(Parent == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Parent)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Parent)}");
+                sb.Append(Parent.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Parent)}");
+            }
+
+            if(NextPart == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(NextPart)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(NextPart)}");
+                sb.Append(NextPart.ToShortString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(NextPart)}");
+            }
 
             return sb.ToString();
         }
@@ -40,7 +67,7 @@ namespace MyNPCLib.PersistLogicalData
         {
             var spaces = StringHelper.Spaces(n);
             var sb = new StringBuilder();
-
+            sb.AppendLine($"{spaces}{nameof(IsActive)} = {IsActive}");
             return sb.ToString();
         }
     }
