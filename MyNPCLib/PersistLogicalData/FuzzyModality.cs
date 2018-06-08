642c3ca247028d301de7f686cd4eb723057d1bbe
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-08 10:13:37 +0300

    snapshot

---
+++ MyNPCLib/PersistLogicalData/FuzzyModality.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNPCLib.PersistLogicalData
+{
+    [Serializable]
+    public class FuzzyModality: ILogicalyAnnotated, IObjectToString, IShortObjectToString
+    {
+        public KindOfModality Kind { get; set; } = KindOfModality.Undefined;
+        public IList<LogicalAnnotation> Annotations { get; set; }
+
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        public string PropertiesToSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            if (Annotations == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Annotations)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Annotations)}");
+                foreach (var annotation in Annotations)
+                {
+                    sb.Append(annotation.ToShortString(nextN));
+                }
+                sb.AppendLine($"{spaces}End {nameof(Annotations)}");
+            }
+            return sb.ToString();
+        }
+
+        public string ToShortString()
+        {
+            return ToShortString(0u);
+        }
+
+        public string ToShortString(uint n)
+        {
+            return this.GetDefaultToShortStringInformation(n);
+        }
+
+        public string PropertiesToShortSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+            sb.AppendLine($"{spaces}{nameof(Kind)} = {Kind}");
+            if (Annotations == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Annotations)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Annotations)}");
+                foreach (var annotation in Annotations)
+                {
+                    sb.Append(annotation.ToShortString(nextN));
+                }
+                sb.AppendLine($"{spaces}End {nameof(Annotations)}");
+            }
+            return sb.ToString();
+        }
+    }
+}
