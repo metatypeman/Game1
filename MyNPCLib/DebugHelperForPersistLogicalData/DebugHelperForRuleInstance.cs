3e5fd67de633e49e69a179334c859ab105943a0d
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-12 17:41:23 +0300

    snapshot

--- MyNPCLib/DebugHelperForPersistLogicalData/DebugHelperForRuleInstance.cs
+++ MyNPCLib/DebugHelperForPersistLogicalData/DebugHelperForRuleInstance.cs
@@ -85,10 +85,134 @@ namespace MyNPCLib.DebugHelperForPersistLogicalData
         private static string ToString(RulePart source, ContextForDebugHelperForRuleInstance context)
         {
             var sb = new StringBuilder();
-            sb.Append($"{{}}");
+            sb.Append("{");
+
+            if(source.Expression != null)
+            {
+                sb.Append(ToString(source.Expression, context));
+            }
+            sb.Append("}");
             return sb.ToString();
         }
 
-        public static string 
+        public static string ToString(BaseExpressionNode source)
+        {
+            var context = new ContextForDebugHelperForRuleInstance();
+            context.MainView = ToString(source, context);
+            return ToString(context);
+        }
+
+        private static string ToString(BaseExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            var kind = source.Kind;
+
+            switch(kind)
+            {
+                case KindOfExpressionNode.And:
+                    return AndToString(source.AsOperatorAnd, context);
+
+                case KindOfExpressionNode.Or:
+                    return OrToString(source.AsOperatorOr, context);
+
+                case KindOfExpressionNode.Not:
+                    return NotToString(source.AsOperatorNot, context);
+
+                case KindOfExpressionNode.Relation:
+                    return RelationToString(source.AsRelation, context);
+
+                case KindOfExpressionNode.Concept:
+                    return ConceptToString(source.AsConcept, context);
+
+                case KindOfExpressionNode.EntityRef:
+                    return EntityRefToString(source.AsEntityRef, context);
+
+                case KindOfExpressionNode.EntityCondition:
+                    return EntityConditionToString(source.AsEntityCondition, context);
+
+                case KindOfExpressionNode.Var:
+                    return VarToString(source.AsVar, context);
+
+                case KindOfExpressionNode.Value:
+                    return ValueToString(source.AsValue, context);
+
+                case KindOfExpressionNode.FuzzyLogicValue:
+                    return FuzzyLogicValueToString(source.AsFuzzyLogicValue, context);
+
+                case KindOfExpressionNode.Fact:
+                    return FactToString(source.AsFact, context);
+
+                default: throw new ArgumentOutOfRangeException(nameof(kind), kind, null);
+            }
+        }
+
+        private static string AndToString(OperatorAndExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            return $"{ToString(source.Left, context)} & {ToString(source.Right, context)}";
+        }
+
+        private static string OrToString(OperatorOrExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            return $"{ToString(source.Left, context)} | {ToString(source.Right, context)}";
+        }
+
+        private static string NotToString(OperatorNotExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            return $"!{ToString(source.Left, context)}";
+        }
+
+        private static string RelationToString(RelationExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{source.Name}(");
+            if(!ListHelper.IsEmpty(source.Params))
+            {
+                var paramsViewsList = new List<string>();
+
+                foreach(var param in source.Params)
+                {
+                    paramsViewsList.Add(ToString(param, context));
+                }
+
+                var paramsStr = string.Join(",", paramsViewsList);
+                sb.Append(paramsStr);
+            }
+            sb.Append(")");
+            return sb.ToString();
+        }
+
+        private static string ConceptToString(ConceptExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            return source.Name;
+        }
+
+        private static string EntityRefToString(EntityRefExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            return source.Name;
+        }
+
+        private static string EntityConditionToString(EntityConditionExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string VarToString(VarExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ValueToString(ValueExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string FuzzyLogicValueToString(FuzzyLogicValueExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string FactToString(FactExpressionNode source, ContextForDebugHelperForRuleInstance context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
