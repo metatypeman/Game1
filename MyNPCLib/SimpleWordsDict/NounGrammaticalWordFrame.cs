9f851dd5857c0865c68340e972d9f6ef2f988876
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-26 17:58:31 +0300

    snapshot

--- MyNPCLib/SimpleWordsDict/NounGrammaticalWordFrame.cs
+++ MyNPCLib/SimpleWordsDict/NounGrammaticalWordFrame.cs
@@ -10,13 +10,18 @@ namespace MyNPCLib.SimpleWordsDict
         public override GrammaticalPartOfSpeech PartOfSpeech => GrammaticalPartOfSpeech.Noun;
         public override bool IsNoun => true;
         public override NounGrammaticalWordFrame AsNoun => this;
-
+        public bool IsName { get; set; }
+        public GrammaticalGender Gender { get; set; }
+        public GrammaticalNumberOfWord Number { get; set; }
         public override string PropertiesToSting(uint n)
         {
             var spaces = StringHelper.Spaces(n);
             var nextN = n + 4;
             var sb = new StringBuilder();
             sb.Append(base.PropertiesToSting(n));
+            sb.AppendLine($"{spaces}{nameof(IsName)} = {IsName}");
+            sb.AppendLine($"{spaces}{nameof(Gender)} = {Gender}");
+            sb.AppendLine($"{spaces}{nameof(Number)} = {Number}");
             return sb.ToString();
         }
     }
