16f789f1647b53571e209e40d40867eb096f2c6d
Sergiy Tolkachov <metatypeman@gmail.com>
2018-06-26 20:50:06 +0300

    snapshot

--- MyNPCLib/SimpleWordsDict/AdverbGrammaticalWordFrame.cs
+++ MyNPCLib/SimpleWordsDict/AdverbGrammaticalWordFrame.cs
@@ -10,13 +10,18 @@ namespace MyNPCLib.SimpleWordsDict
         public override GrammaticalPartOfSpeech PartOfSpeech => GrammaticalPartOfSpeech.Adverb;
         public override bool IsAdverb => true;
         public override AdverbGrammaticalWordFrame AsAdverb => this;
-
+        public GrammaticalComparison Comparison { get; set; } = GrammaticalComparison.None;
+        public bool IsQuestionWord { get; set; }
+        public bool IsDeterminer { get; set; }
         public override string PropertiesToSting(uint n)
         {
             var spaces = StringHelper.Spaces(n);
             var nextN = n + 4;
             var sb = new StringBuilder();
             sb.Append(base.PropertiesToSting(n));
+            sb.AppendLine($"{spaces}{nameof(Comparison)} = {Comparison}");
+            sb.AppendLine($"{spaces}{nameof(IsQuestionWord)} = {IsQuestionWord}");
+            sb.AppendLine($"{spaces}{nameof(IsDeterminer)} = {IsDeterminer}");
             return sb.ToString();
         }
     }
--- MyNPCLib/SimpleWordsDict/NumeralGrammaticalWordFrame.cs
+++ MyNPCLib/SimpleWordsDict/NumeralGrammaticalWordFrame.cs
@@ -10,13 +10,14 @@ namespace MyNPCLib.SimpleWordsDict
         public override GrammaticalPartOfSpeech PartOfSpeech => GrammaticalPartOfSpeech.Numeral;
         public override bool IsNumeral => true;
         public override NumeralGrammaticalWordFrame AsNumeral => this;
-
+        public NumeralType NumeralType { get; set; }
         public override string PropertiesToSting(uint n)
         {
             var spaces = StringHelper.Spaces(n);
             var nextN = n + 4;
             var sb = new StringBuilder();
             sb.Append(base.PropertiesToSting(n));
+            sb.AppendLine($"{spaces}{nameof(NumeralType)} = {NumeralType}");
             return sb.ToString();
         }
     }
