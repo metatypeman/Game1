1b52290d9f6b5d5a5fc72e7d457396d661e61a67
Sergiy Tolkachov <metatypeman@gmail.com>
2018-09-05 06:29:38 +0300

    snapshot

--- MyNPCLib/LogicalHostEnvironment/BusOfCGStorages.cs
+++ MyNPCLib/LogicalHostEnvironment/BusOfCGStorages.cs
@@ -5,37 +5,40 @@ using System.Text;
 
 namespace MyNPCLib.LogicalHostEnvironment
 {
-    public class BusOfCGStorages
+    public class BusOfCGStorages: IBusOfCGStorages
     {
         private readonly object mStoragesDictLockObj = new object();
         private Dictionary<ulong, ICGStorage> mStoragesDict = new Dictionary<ulong, ICGStorage>();
 
-        public void AddStorage(ulong entityId, ICGStorage storage)
+        public void AddStorage(IHostLogicalObjectStorageForBus storage)
         {
             lock(mStoragesDictLockObj)
             {
-                mStoragesDict[entityId] = storage;
+                throw new NotImplementedException();
+                //mStoragesDict[entityId] = storage;
             }
         }
 
-        public void RemoveStorage(ulong entityId)
+        public void RemoveStorage(ulong entityKey)
         {
             lock (mStoragesDictLockObj)
             {
-                mStoragesDict.Remove(entityId);
+                throw new NotImplementedException();
+                //mStoragesDict.Remove(entityId);
             }
         }
 
-        public ICGStorage GetStorage(ulong entityId)
+        public ICGStorage GetStorage(ulong entityKey)
         {
             lock (mStoragesDictLockObj)
             {
-                if(mStoragesDict.ContainsKey(entityId))
-                {
-                    return mStoragesDict[entityId];
-                }
+                throw new NotImplementedException();
+                //if(mStoragesDict.ContainsKey(entityId))
+                //{
+                //    return mStoragesDict[entityId];
+                //}
 
-                return null;
+                //return null;
             }
         }
     }
--- MyNPCLib/LogicalHostEnvironment/HostLogicalObjectStorage.cs
+++ MyNPCLib/LogicalHostEnvironment/HostLogicalObjectStorage.cs
@@ -1,11 +1,13 @@
 ﻿using MyNPCLib.CGStorage;
+using MyNPCLib.PersistLogicalData;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyNPCLib.LogicalHostEnvironment
 {
-    public class HostLogicalObjectStorage: BaseProxyStorage
+    public class HostLogicalObjectStorage: BaseProxyStorage, IHostLogicalObjectStorageForBus
     {
         public HostLogicalObjectStorage(IEntityDictionary entityDictionary)
             : base(entityDictionary)
@@ -13,5 +15,79 @@ namespace MyNPCLib.LogicalHostEnvironment
         }
 
         public override KindOfCGStorage KindOfStorage => KindOfCGStorage.OtherProxy;
+        private readonly object mLockObj = new object();
+        private ulong mEntityId;
+        private DefaultHostCGStorage mGeneralHost;
+        private DefaultHostCGStorage mVisibleHost;
+        private DefaultHostCGStorage mPublicHost;
+
+        public override void Append(RuleInstance ruleInstance)
+        {
+            lock(mLockObj)
+            {
+#if DEBUG
+                LogInstance.Log($"ruleInstance = {ruleInstance}");
+#endif
+
+                var listOfKindOfAcessPolicy = ruleInstance.AccessPolicyToFactModality.Select(p => p.Kind).ToList();
+
+#if DEBUG
+                LogInstance.Log($"listOfKindOfAcessPolicy.Count = {listOfKindOfAcessPolicy.Count}");
+                foreach(var kindOfAcessPolicy in listOfKindOfAcessPolicy)
+                {
+                    LogInstance.Log($"kindOfAcessPolicy = {kindOfAcessPolicy}");
+                }
+#endif
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void Append(ICGStorage storage)
+        {
+            lock (mLockObj)
+            {
+#if DEBUG
+                LogInstance.Log($"storage = {storage}");
+#endif
+                var mainRuleInstance = storage.MainRuleInstance;
+
+                var listOfKindOfAcessPolicy = mainRuleInstance.AccessPolicyToFactModality.Select(p => p.Kind).ToList();
+
+#if DEBUG
+                LogInstance.Log($"listOfKindOfAcessPolicy.Count = {listOfKindOfAcessPolicy.Count}");
+                foreach (var kindOfAcessPolicy in listOfKindOfAcessPolicy)
+                {
+                    LogInstance.Log($"kindOfAcessPolicy = {kindOfAcessPolicy}");
+                }
+#endif
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void Append(RuleInstancePackage ruleInstancePackage)
+        {
+            lock (mLockObj)
+            {
+#if DEBUG
+                LogInstance.Log($"ruleInstancePackage = {ruleInstancePackage}");
+#endif
+
+                var mainRuleInstance = ruleInstancePackage.MainRuleInstance;
+
+                var listOfKindOfAcessPolicy = mainRuleInstance.AccessPolicyToFactModality.Select(p => p.Kind).ToList();
+
+#if DEBUG
+                LogInstance.Log($"listOfKindOfAcessPolicy.Count = {listOfKindOfAcessPolicy.Count}");
+                foreach (var kindOfAcessPolicy in listOfKindOfAcessPolicy)
+                {
+                    LogInstance.Log($"kindOfAcessPolicy = {kindOfAcessPolicy}");
+                }
+#endif
+
+                throw new NotImplementedException();
+            }
+        }
     }
 }
