a402052b8ee010eddf769980d7e0972eeefc5146
Sergiy Tolkachov <metatypeman@gmail.com>
2021-01-03 22:51:18 +0200

    sprint 9 snapshot

---
+++ Assets/SymOntoClay/Scriptables/NPCFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SymOntoClay.Scriptables
+{
+    public class NPCFile : ScriptableObject
+    {
+        public string FullName;
+    }
+}
