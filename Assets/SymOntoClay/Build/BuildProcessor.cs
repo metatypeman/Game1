22e9d3c566b3497428433a3383f3dc7d7526b7ce
Sergiy Tolkachov <metatypeman@gmail.com>
2021-01-08 18:49:49 +0200

    sprint 9 BuildPipeline

---
+++ Assets/SymOntoClay/Build/BuildProcessor.cs
@@ -0,0 +1,34 @@
+﻿using SymOntoClay.UnityAsset.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if UNITY_EDITOR
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+#endif
+using UnityEngine;
+
+namespace Assets.SymOntoClay.Build
+{
+#if UNITY_EDITOR
+    public class BuildProcessor : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        /// <summary>
+        /// This method is called after build (publish) game and before finish.
+        /// You can write your code for copying target files here.
+        /// </summary>
+        /// <param name="report"></param>
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            Debug.Log($"report.summary.outputPath = {report.summary.outputPath}");
+            Debug.Log($"Application.dataPath = {Application.dataPath}");
+
+            BuildPipeLine.CopyFiles(Application.dataPath, report.summary.outputPath);
+        }
+    }
+#endif
+}
--- Assets/SymOntoClay/Helpers/FileHelper.cs
+++ Assets/SymOntoClay/Helpers/FileHelper.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEditor;
-using UnityEditor.Experimental.AssetImporters;
 
 namespace SymOntoClay.Helpers
 {
