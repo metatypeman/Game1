c4fb92a0a81d52599bbff9e8f53ff296241e2e80
Sergiy Tolkachov <metatypeman@gmail.com>
2021-01-09 16:53:18 +0200

    sprint 10 test moving to point in relative polar coordinates.

--- Assets/TstNPCBehaviourScript.cs
+++ Assets/TstNPCBehaviourScript.cs
@@ -22,11 +22,53 @@ public class TstNPCBehaviourScript : MonoBehaviour
 
         //_navMeshAgent.SetDestination(position);
 
-        var forvard = transform.forward;
+        //        var forvard = transform.forward;
+
+        //#if DEBUG
+        //        Debug.Log($"TstNPCBehaviourScript Start forvard = {forvard}");
+        //#endif
+
+        //        var normalizedForvard = forvard.normalized;
+
+        //#if DEBUG
+        //        Debug.Log($"TstNPCBehaviourScript Start normalizedForvard = {normalizedForvard}");
+        //#endif
+
+        //        var distance = 5;
+
+        //        var newLocalPosition = new Vector3(normalizedForvard.x * distance, normalizedForvard.y, normalizedForvard.z);
+
+        //#if DEBUG
+        //        Debug.Log($"TstNPCBehaviourScript Start newLocalPosition = {newLocalPosition}");
+        //#endif
+
+        //        var newGlobalPosition = transform.TransformVector(newLocalPosition);
+
+        //#if DEBUG
+        //        Debug.Log($"TstNPCBehaviourScript Start newGlobalPosition = {newGlobalPosition}");
+        //#endif
+
+        //        cube1.transform.position = newGlobalPosition;
+
+        //var angle = 45;
+        var angle = 0;
+        var distance = 5;
+
+        var radAngle = angle * Mathf.Deg2Rad;
+        var x = Mathf.Sin(radAngle);
+        var y = Mathf.Cos(radAngle);
+        var localDirection = new Vector3(x * distance, 0f, y * distance);
 
 #if DEBUG
-        Debug.Log($"TstNPCBehaviourScript Start forvard = {forvard}");
+        Debug.Log($"TstNPCBehaviourScript Start localDirection = {localDirection}");
 #endif
+
+        var globalDirection = transform.TransformDirection(localDirection);
+
+        var newPosition = globalDirection + transform.position;
+
+        //cube1.transform.position = newPosition;
+        _navMeshAgent.SetDestination(newPosition);
     }
 
     // Update is called once per frame
